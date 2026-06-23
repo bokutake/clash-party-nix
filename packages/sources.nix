0af6e81405a704e1825684005b00ec3af7ab6a39
bokutake <i@html.moe>
2026-06-23 23:48:39 +0800

    v1.9.6

--- packages/sources.nix
+++ packages/sources.nix
@@ -12,6 +12,15 @@
   #   version = "1.9.6";
   #   path = /absolute/path/to/clash-party-linux-1.9.6-amd64.tar.xz;
   # };
-  x86_64-linux = null;
-  aarch64-linux = null;
+  x86_64-linux = {
+    version = "1.9.6";
+    url = "https://github.com/bokutake/clash-party-nix/releases/download/v1.9.6/clash-party-linux-1.9.6-amd64.tar.xz";
+    hash = "sha256-7ZkWsl60iB9T5IuhT9ROZ/azk/oF7mmnt0BkCt8ME70=";
+  };
+
+  aarch64-linux = {
+    version = "1.9.6";
+    url = "https://github.com/bokutake/clash-party-nix/releases/download/v1.9.6/clash-party-linux-1.9.6-arm64.tar.xz";
+    hash = "sha256-R2PRvu0kGioACmlwzvvuZ/kQt8axQPre694tNyLowXs=";
+  };
 }
